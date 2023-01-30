d1ee4924797bc483d4945757fb66acc567ea5f1a
vrnsky <vrnsky@protonmail.ch>
2023-01-30 20:26:17 +0800

    GoLang A to Z. Add basics - declarations of variables

---
+++ chapter1/Basics.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	and := true && false
+	fmt.Println(and)
+
+	or := true || false
+	fmt.Println(or)
+
+	not := !true
+	fmt.Println(not)
+
+	year := 2023
+	fmt.Println(year)
+
+	rate := 1.25
+	fmt.Println(rate)
+
+	terms := "Terms"
+	fmt.Println(terms)
+
+}
