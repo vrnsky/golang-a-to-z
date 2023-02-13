d4b156e34bee262eac986aaba774596991618c1b
vrnsky <vrnsky@protonmail.ch>
2023-02-13 19:05:26 +0800

    GoLang A to Z. Add a few unit test for one function

--- chapter4/gordle/game.go
+++ chapter4/gordle/game.go
@@ -1,11 +1,22 @@
 package gordle
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
 
-type Game struct{}
+const wordLength = 5
 
-func New() *Game {
-	g := &Game{}
+type Game struct {
+	reader *bufio.Reader
+}
+
+func New(playerInput io.Reader) *Game {
+	g := &Game{
+		reader: bufio.NewReader(playerInput),
+	}
 	return g
 }
 
@@ -13,4 +24,24 @@ func (g *Game) Play() {
 	fmt.Println("Welcome to Gordle!")
 
 	fmt.Printf("Enter a guess:\n")
+}
+
+// ask reads input until a valid suggestion is made (and returned).
+func (g *Game) ask() []rune {
+	fmt.Printf("Enter a %d-character guess:\n", wordLength)
+
+	for {
+		playerInput, _, err := g.reader.ReadLine()
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Gordle failed to read your guess: %s\n", err.Error())
+		}
+
+		guess := []rune(string(playerInput))
+
+		if len(guess) != wordLength {
+			_, _ = fmt.Fprintf(os.Stderr, "Your attempt is invalid with with Gordle's solution! Expected %d character, got %d.", wordLength, len(guess))
+		} else {
+			return guess
+		}
+	}
 }
