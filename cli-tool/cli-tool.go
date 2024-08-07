93dca45588a4364e195b5aa315d87b5c8e0197a5
vrnsky <vrnsky@protonmail.ch>
2024-08-08 01:09:11 +0800

    feat: GLAZ-1. Fix parsing command line interface flags

--- cli-tool/cli-tool.go
+++ cli-tool/cli-tool.go
@@ -10,6 +10,7 @@ func main() {
 	namespace := flag.String("namespace", "minikube", "Namespace of K8s")
 	serviceName := flag.String("service", "mesh-service", "Name of the service")
 
+	flag.Parse()
 	fmt.Printf("Start checking services. Base url: %v\n", *baseUrl)
 	fmt.Printf("Namespace: %v\n", *namespace)
 	fmt.Printf("Checking service [%v]\n", *serviceName)
